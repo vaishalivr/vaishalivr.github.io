22086cf0d3391092bacaefa680f1e06622be1519
Vaishali Verma <65554439+vaishalivr@users.noreply.github.com>
2021-03-08 14:10:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-//var width = 300;
-//var height = 300;
-    
-	//var svg = d3.select('svg')
-		//.attr('width', width)
-		//.attr('height', height)
-		//.attr('transform', 'translate(600,0)')
-		//.append('g')
-		//.attr('class', 'phyllotaxis');
-	    
-	//var radius = 3;
-	//var points = d3.range(600).map(phyllotaxis(6));
-	    
-	//svg.selectAll('.point').data(points)
-		//.enter()
-		//.append('circle')
-		//.transition()
-		//.duration(3000)
-  		//.attr("cx", function(d) { return d[0] + radius; })
-	  	//.attr("cy", function(d) { return d[1]; })
-  		//.attr("r", radius)
-  		//.style("fill", "#008800");
-	    
-	 //function phyllotaxis(radius) {
-    		//var theta = Math.PI * (3 - Math.sqrt(5));
-    		//return function(i) {
-      			//var r = radius * Math.sqrt(i), a = theta * i;
-      			//return [
-        			//width / 2 + r * Math.cos(a),
-        			//height / 2 + r * Math.sin(a)
-      //];
-   // };
- // }
-
 var width = 300,
     height = 300,
     radius = 100;
